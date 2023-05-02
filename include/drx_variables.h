77a0723cc4e1d08e09f94ccade49775bafe85d42
Nimesh Khandelwal <nimesh6798@gmail.com>
2023-05-02 17:25:01 +0530

    variable and function declarations for utility functions

---
+++ include/drx_variables.h
@@ -0,0 +1,12 @@
+#ifndef DRX_VARIABLES
+#define DRX_VARIABLES
+
+#define STATES 50
+#define GRID_SIZE 1000
+#define ITERATIONS_MC 300000
+#define STRAIN_RATE 0.01
+#define CELL_SIZE 2
+#define ff(i, n) for (int i = 0; i < n; i++)
+#define fb(i, n) for (int i = n; i >= 0; i--)
+
+#endif
